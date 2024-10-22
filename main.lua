05a752c4de6b9cf0e1d6a5268924c4d8cefc4efc
CLP Development <115387383+CLPDevelopment@users.noreply.github.com>
2024-10-22 23:14:51 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,23 +1,19 @@
--- Import this library
-local GUI = require("GUI")
--- We will also need downloaded double buffering library to render rectangles
-local screen = require("Screen")
+-- GET request
+internet.request("http://httpbin.org/ip")
 
---------------------------------------------------------------------------------
+-- POST request
+internet.request(
+    "http://httpbin.org/post",
+    "My POST data",
+    {
+    	["User-Agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0"
+    },
+)
 
--- Create new workspace
-local workspace = GUI.workspace()
-
--- Create and add template object to workspace
-local object = workspace:addChild(GUI.object(3, 2, 50, 10))
--- Create own :draw() method and make it render green rectangle
-object.draw = function(object)
-	screen.drawRectangle(object.x, object.y, object.width, object.height, 0x33FF80, 0x0, " ")
-end
-
---------------------------------------------------------------------------------
-
--- Draw workspace content once on screen when program starts
-workspace:draw()
--- Start processing events for workspace
-workspace:start()
+-- HEAD request
+internet.request(
+    "https://httpbin.org/headers",
+    nil,
+    nil,
+    "HEAD"
+)
